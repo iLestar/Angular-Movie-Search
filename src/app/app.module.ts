ecd217aee309e53595bad2499210c2848f1885c8
iLestar <juanlestar0408@gmail.com>
2023-08-26 12:39:07 -0500

    Imports added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InputComponent } from './input/input.component';
-import { SearchDetailComponent } from './search-detail/search-detail.component';
+import { InputComponent } from './components/input/input.component';
+import { SearchDetailComponent } from './components/search-detail/search-detail.component';
 
 @NgModule({
   declarations: [
